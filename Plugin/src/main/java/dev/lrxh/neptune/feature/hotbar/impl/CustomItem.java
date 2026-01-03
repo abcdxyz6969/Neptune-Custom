b7bf512ddacd33b51b30a11f00e073ac45b393a5
mind <rioeyuzenin@gmail.com>
2026-01-03 15:43:24 +0700

    Update CustomItem.java

--- Plugin/src/main/java/dev/lrxh/neptune/feature/hotbar/impl/CustomItem.java
+++ Plugin/src/main/java/dev/lrxh/neptune/feature/hotbar/impl/CustomItem.java
@@ -6,10 +6,14 @@ import java.util.List;
 
 @Getter
 public class CustomItem extends Item {
-    private final List<String> commands;
 
-    public CustomItem(String displayName, String material, List<String> lore, byte slot, List<String> commands, int customModelData) {
+    private final List<String> commands;
+    private final List<String> consoleCommands;
+
+    public CustomItem(String displayName, String material, List<String> lore, byte slot,
+                      List<String> commands, List<String> consoleCommands, int customModelData) {
         super(null, displayName, material, lore, true, slot, customModelData);
         this.commands = commands;
+        this.consoleCommands = consoleCommands;
     }
 }
